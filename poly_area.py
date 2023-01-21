d5fb109d85ba3b72b49a3d87596f225335faf2e4
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-01-21 15:33:06 -0500

    feat: built square class

--- poly_area.py
+++ poly_area.py
@@ -40,3 +40,10 @@ class Square(Rectangle):
     def __init__(self, side):
         self.width = side
         self.height = side
+
+    def __str__(self):
+        return f'Square(side={self.width})'
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
