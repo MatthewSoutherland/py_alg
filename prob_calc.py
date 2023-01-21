cee64cc66e9b34ee85f81eaa5cbd3a9805793f60
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-01-21 16:05:17 -0500

    feat: done

--- prob_calc.py
+++ prob_calc.py
@@ -15,7 +15,26 @@ class Hat():
     def draw(self, number):
         all_removed = []
         if (number > len(self.contents)):
+            return self.contents
+        for i in range(number):
             removed = self.contents.pop(
+                # every time you pop the length will change, so by setting self.contents will adjust on each pop.
                 int(random.random() * len(self.contents)))
             all_removed.append(removed)
         return all_removed
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    count = 0
+    for i in range(num_experiments):
+        expected_copy = copy.deepcopy(expected_balls)
+        hat_copy = copy.deepcopy(hat)
+        colors_gotten = hat_copy.draw(num_balls_drawn)
+
+        for color in colors_gotten:
+            if (color in expected_copy):
+                expected_copy[color] -= 1
+
+    if (all(x <= 0 for x in expected_copy.values())):
+        count += 1
+    return count / num_experiments
