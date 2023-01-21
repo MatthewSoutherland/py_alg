6250f2323f6f946b06d872b7aa9a644512620163
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-01-21 15:28:46 -0500

    feat: built rectangle class

---
+++ poly_area.py
@@ -0,0 +1,44 @@
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(height):
+        height = height
+
+    def get_area(self):
+        area = self.width * self.height
+        return area
+
+    def get_perimeter(self):
+        perimeter = self.width * 2 + self.height * 2
+        return perimeter
+
+    def get_diagonal(self):
+        diagonal = ((self.width ** 2 + self.height ** 2) ** .5)
+        return diagonal
+
+    def get_picture(self):
+        if (self.width > 50 or self.height > 50):
+            return "Too big for picture."
+        string = (("*") * self.width) + "\n" * self.height
+        return string
+
+    def get_amount_inside(self, shape):
+        return int(self.get_area() / shape.get_area())
+
+
+class Square(Rectangle):
+
+    def __init__(self, side):
+        self.width = side
+        self.height = side
+
+
+box = Rectangle(5, 10)
+
+print(dir(box))
